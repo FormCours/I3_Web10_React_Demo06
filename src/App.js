d4306c9a2e233f2219af59974150d411e3eb7ccc
Aurélien <aurelien.strimelle@bstorm.be>
2022-02-07 10:48:48 +0100

    Refactoring des console.log

--- src/App.js
+++ src/App.js
@@ -6,16 +6,26 @@ function App() {
 
   const [display, setDisplay] = useState(true);
 
+  useEffect(() => {
+    console.log('Effect: App !');
+
+    return () => {
+      console.log('Effect: Clear App !');
+    };
+  });
+
   const handleToggleDisplay = () => {
+    console.log('Bouton: Jour/Nuit');
     setDisplay(d => !d);
-  }
+  };
 
   return (
     <div className="App">
+      {console.log('Render App')}
       <h1>Demo 06 - Le cycle de vie</h1>
 
       <button onClick={handleToggleDisplay}>Jour/Nuit</button>
-      {display &&(
+      {display && (
         <Counter />
       )}
 
--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -5,22 +5,29 @@ const Counter = () => {
 
     useEffect(() => {
         // Effet pour l'ajout et la mise a jours
-        console.log('Effect start !');
+        console.log('Effect: Counter !');
+
+        // Action sur le DOM
         document.title = `Counter : ${count}`;
 
         // Effet de nettoyage
         return () => {
-            console.log('Effect Clear :o');
+            console.log('Effect: Clear Counter !:o');
+
+            // Action sur le DOM
+            document.title = `Counter remove :(`;
         }
     })
 
     const handleUpCounter = () => {
+        console.log('Bouton: Counter ++');
+
         // Mise à jour du state => Demande a react d'actualiser le composant
         setCount(c => c + 1);
     }
 
     return (<>
-        {console.log('Render')}
+        {console.log('Render: Counter')}
         <p>Valeur du compteur : {count}</p>
         <button onClick={handleUpCounter}>+1</button>
     </>)
